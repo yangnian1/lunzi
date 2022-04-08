49af71ec0f6d24c87900d52b80f9878ea2dc2b44
460530214@qq.com <460530214@qq.com>
2022-04-08 17:14:19 +0800

    test

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <F>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import F from './components/f.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    F,
+  }
+}
+</script>
